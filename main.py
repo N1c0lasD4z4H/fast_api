15b8085ee0d3f689b9f7d95c8572858294962495
Nicolas Daza Hernandez <ndaza@adgasd32.int>
2025-02-14 09:51:31 -0500

    Validación de datos y documentacion mas facil de leer

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional, List
+import datetime
 app = FastAPI()
 
 #Consualtar datos
@@ -13,6 +14,35 @@ class Movie(BaseModel):
     rating: float  
     categoria: str 
 
+class MovieCreate(BaseModel):
+    id: int
+    title: str = Field(min_length=5, max_length=15)  
+    overview: str = Field(min_length=15, max_length=60)
+    year: int  =Field(le=datetime.date.today().year, ge=1999)
+    rating: float  = Field(ge=1, le=10)
+    categoria: str = Field(min_length=5, max_length=20)
+#gt greater than
+#ge greater than or equal
+#lt less than
+#le less than or equal menor o igual
+
+
+# datos por defecto
+    model_config = {
+        'json_schema_extra':{
+            'example':{
+                'id':1,
+                'title':'My movie',
+                'overview':'esta vaina es seria compare..',
+                'year':2000,
+                'rating':8,
+                'categoria':'Acción'
+            }
+        }
+    }
+
+
+
 class MovieUpdate(BaseModel):
     title: str  
     overview: str  
@@ -21,26 +51,7 @@ class MovieUpdate(BaseModel):
     categoria: str 
 
 
-movies = [
-    {
-        "id":1,
-        "title":"Avatar",
-        "overview":"En un exuberante planeta llamado pandora viven los Na vi ...",
-        "year": "2009",
-        "rating": 7.9,
-        "category":"Accion"
-
-    },
-    {
-        "id":2,
-        "title":"Avater",
-        "overview":"En un exuberante planeta llamado pandora viven los Na vi ...",
-        "year": "2009",
-        "rating": 7.9,
-        "category":"Comedia*"
-
-    }
-]
+movies: List[Movie] = []
 
 @app.get("/", tags=['Home'])
 async def home():
@@ -48,14 +59,14 @@ async def home():
 
 @app.get("/movies", tags=['Movies'])
 async def get_movies() -> List[Movie]:
-    return movies
+    return [movie.model_dump()for movie in movies]
 
 @app.get("/movies/{id}", tags=['Movies'])
 
 async def get_movie(id: int) -> Movie:
     for movie in movies:
         if movie["id"] == id:
-            return movie
+            return movie.model_dump()
     return[]
     
 @app.get("/movies/", tags=['Movies'])
@@ -63,16 +74,16 @@ async def get_movie(id: int) -> Movie:
 async def get_movie_by_category(category: str, year: int) -> Movie:
      for movie in movies:
         if movie["category"] == category:
-            return movie
+            return movie.model_dump()
         
      return[]
 
 @app.post('/movies', tags=['Movies'])
 
-def create_movie(movie: Movie)-> List[Movie]:
+def create_movie(movie: MovieCreate)-> List[Movie]:
 
-    movies.append(movie.model_dump())
-    return movies
+    movies.append(movie)
+    return [movie.model_dump()for movie in movies]
 
 @app.put('/movies/{id}', tags=['Movies'])
 def update_movie(id: int, movie: MovieUpdate)-> List[Movie]:
@@ -83,11 +94,11 @@ def update_movie(id: int, movie: MovieUpdate)-> List[Movie]:
             item['year'] = movie.year
             item['rating'] = movie.rating
             item['category'] = movie.categoria
-  return movies
+  return [movie.model_dump()for movie in movies]
 
 @app.delete('/movies/{id}', tags=['Movies'])
 def delete_movie(id: int)-> List[Movie]:
     for movie in movies:
         if movie['id'] == id:
             movies.remove(movie)
-    return movies
+    return [movie.model_dump()for movie in movies]
